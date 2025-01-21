3f7aea3f393fc9ac255a4c307298bcf5003a7750
biryanim <ayunusov42@gmail.com>
2025-01-21 19:29:52 +0300

    implement api layout

--- cmd/main.go
+++ cmd/main.go
@@ -3,83 +3,19 @@ package main
 import (
 	"context"
 	"flag"
+	userAPI "github.com/biryanim/auth/internal/api/user"
 	"github.com/biryanim/auth/internal/config"
-	"github.com/biryanim/auth/internal/converter"
 	userRepository "github.com/biryanim/auth/internal/repository/user"
-	"github.com/biryanim/auth/internal/service"
 	userService "github.com/biryanim/auth/internal/service/user"
 	desc "github.com/biryanim/auth/pkg/user_api_v1"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
-	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/types/known/emptypb"
 	"log"
 	"net"
 )
 
 var configPath string
-
-type server struct {
-	desc.UnimplementedUserAPIV1Server
-	userService service.UserService
-}
-
-func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
-	if req.GetPassword() != req.GetPassword() {
-		return nil, status.Error(codes.InvalidArgument, "password does not match")
-	}
-
-	id, err := s.userService.Create(ctx, converter.ToUserInfoFromDesc(req.GetInfo()))
-	if err != nil {
-		return nil, err
-	}
-
-	log.Printf("inserted user with id: %d", id)
-
-	return &desc.CreateResponse{
-		Id: id,
-	}, nil
-}
-
-func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
-	if req.GetId() == 0 {
-		return nil, status.Error(codes.InvalidArgument, "invalid id")
-	}
-
-	err := s.userService.Update(ctx, req.GetId(), converter.ToUpdatedUserInfoFromDesc(req.GetInfo()))
-	if err != nil {
-		return nil, err
-	}
-	return &emptypb.Empty{}, nil
-}
-
-func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
-	if req.GetId() == 0 {
-		return nil, status.Error(codes.InvalidArgument, "invalid id")
-	}
-
-	err := s.userService.Delete(ctx, req.GetId())
-	if err != nil {
-		return nil, err
-	}
-
-	return &emptypb.Empty{}, nil
-}
-
-func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
-	userObj, err := s.userService.Get(ctx, req.Id)
-	if err != nil {
-		return nil, err
-	}
-
-	log.Printf("id: %d,	name: %s,	email: %s,	role: %v,	created_at: %v,	updated_at: %v,", userObj.ID, userObj.Info.Name, userObj.Info.Email, userObj.Info.Role, userObj.CreatedAt, userObj.UpdatedAt)
-
-	return &desc.GetResponse{
-		User: converter.ToUserFromService(userObj),
-	}, nil
-}
 
 func init() {
 	flag.StringVar(&configPath, "config-path", ".env", "path to config file")
@@ -120,7 +56,7 @@ func main() {
 
 	s := grpc.NewServer()
 	reflection.Register(s)
-	desc.RegisterUserAPIV1Server(s, &server{userService: userSrv})
+	desc.RegisterUserAPIV1Server(s, userAPI.NewImplementation(userSrv))
 
 	log.Printf("server listening on %s", lis.Addr())
 
